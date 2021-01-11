6351cd95d40acd5b83b25b5a146788c0d3bb9650
Rohit22s <66766075+Rohit22s@users.noreply.github.com>
2021-01-11 21:35:45 +0545

    Commit

---
+++ container.js
@@ -0,0 +1,43 @@
+const dependable = require('dependable');
+const path = require('path');
+
+const container = dependable.container();
+
+const simpleDependecies = [
+    ['_', 'lodash'],
+    ['passport', 'passport'],
+    ['validator', 'express-validator'],
+    ['Clubs','./models/clubs.js'],
+    ['aws','./helpers/AWSUpload.js']
+];
+
+simpleDependecies.forEach(function(val){
+   container.register(val[0], function(){
+       return require(val[1]);
+   }) 
+});
+
+container.load(path.join(__dirname, '/controllers'));
+container.load(path.join(__dirname, '/helpers'));
+
+container.register('container', function(){
+    return container;
+});
+
+module.exports = container;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
